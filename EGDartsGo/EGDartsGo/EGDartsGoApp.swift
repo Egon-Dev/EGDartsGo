5080a694b2a4273916a2e3dea5529a1e27936fbc
changmuk.im <changmuk.im@phoenixdarts.com>
2023-12-27 17:30:08 +0900

    [no] feat: Create SwiftUI project

---
+++ EGDartsGo/EGDartsGo/EGDartsGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EGDartsGoApp.swift
+//  EGDartsGo
+//
+//  Created by changmuk.im@phoenixdarts.com on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct EGDartsGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
